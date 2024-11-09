de8eaf6359ad4e65d140c045c3680a57116dd2b8
ThisIsJyotsna <jyotsnarao11.jr@gmail.com>
2024-11-09 14:15:43 +0530

    9th Nov commit

---
+++ Nov/ex_09112024_Literals_Variables/Lab030_Task2.py
@@ -0,0 +1,8 @@
+num1=int(input("Enter the first number"))
+num2=int(input("Enter the second number"))
+
+rem=num1%num2
+quo=num1//num2
+
+print("Remainder of two numbers is:",rem)
+print("Quotient of two numbers is:",quo)
