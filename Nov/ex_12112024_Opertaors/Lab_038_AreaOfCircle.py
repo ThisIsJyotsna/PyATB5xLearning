1849a2b4a6fef92f3f6901936204c538da01739d
ThisIsJyotsna <jyotsnarao11.jr@gmail.com>
2024-11-12 10:10:40 +0530

    12th Nov commit

---
+++ Nov/ex_12112024_Opertaors/Lab_038_AreaOfCircle.py
@@ -0,0 +1,17 @@
+import math
+
+r=float(input("Enter the radius of a circle"))
+piValue=math.pi
+radiusSquare=pow(r,2)
+#print(radiusSquare)
+area=piValue*radiusSquare
+print(area)
+
+if area>10:
+    print("area is greater than 10")
+
+elif area==10:
+    print("area is equal to 10")
+
+else:
+    print("area is less than 10")
